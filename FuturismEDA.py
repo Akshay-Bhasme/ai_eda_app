43a8ce6d5c7cf40174edf183886ae684945563e8
Akshay Bhasme <akshaybhasme30@gmail.com>
2024-01-09 12:17:37 +0530

    Update FuturismEDA.py

--- FuturismEDA.py
+++ FuturismEDA.py
@@ -22,6 +22,7 @@ import langchain_experimental
 from langchain_experimental.agents import create_pandas_dataframe_agent
 from langchain.llms import OpenAI
 import sweetviz as sv
+import tempfile
 #from streamlit.server.server import Server
 
 class SessionState:
