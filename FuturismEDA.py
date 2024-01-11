adce99348e34083a31cc1ef6708358d9cd0f2eed
Akshay Bhasme <akshaybhasme30@gmail.com>
2024-01-11 12:34:16 +0530

    Update FuturismEDA.py

--- FuturismEDA.py
+++ FuturismEDA.py
@@ -95,7 +95,8 @@ if st.button("Run the analysis"):
     st.image("chart.png")
     image = PIL.Image.open('chart.png')
     vision_model = genai.GenerativeModel('gemini-pro-vision')
-    response = vision_model.generate_content(["What are the observations and analysis from this graph can be made? Write it in simple text format",image])
+    response = vision_model.generate_content(["What are the observations and analysis from this graph can be made? Write it in simple text format",image],generation_config=genai.types.GenerationConfig(
+                                  candidate_count=1))
     st.write(response.text)
         
     # Univariate pdf plots for numeric variables
@@ -106,7 +107,8 @@ if st.button("Run the analysis"):
         st.write("Univariate plot for {}".format(column))
         st.image('univariate.png')
         image = PIL.Image.open('univariate.png')
-        response = vision_model.generate_content(["What are the observations and analysis from this graph can be made? Write it in simple text format",image])
+        response = vision_model.generate_content(["What are the observations and analysis from this graph can be made? Write it in simple text format",image],generation_config=genai.types.GenerationConfig(
+                                  candidate_count=1))
         st.write(response.text)
 
 
