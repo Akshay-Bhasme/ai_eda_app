799f0b868b8bec59d3906cef221d4fcb035e2af5
Akshay Bhasme <akshaybhasme30@gmail.com>
2024-01-11 10:54:01 +0530

    Update FuturismEDA.py

--- FuturismEDA.py
+++ FuturismEDA.py
@@ -9,6 +9,7 @@ warnings.filterwarnings("ignore")
 import os
 import streamlit as st
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from getpass import getpass
